1ec5eefbc3865992a67d67324de7df64706f5731
Roman Petrenko <rp@voltmobi.com>
2015-12-10 01:11:06 +0300

    fixed different menu support

--- library/src/main/java/com/baoyz/swipemenulistview/SwipeMenuAdapter.java
+++ library/src/main/java/com/baoyz/swipemenulistview/SwipeMenuAdapter.java
@@ -49,6 +49,7 @@ public class SwipeMenuAdapter implements WrapperListAdapter,
         if (convertView == null) {
             View contentView = mAdapter.getView(position, convertView, parent);
             SwipeMenu menu = new SwipeMenu(mContext);
+            menu.setViewType(getItemViewType(position));
             createMenu(menu);
             SwipeMenuView menuView = new SwipeMenuView(menu,
                     (SwipeMenuListView) parent);
